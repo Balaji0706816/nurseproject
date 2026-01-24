4df448b222d3d3ec96fdac7e149f17d1916f1fdc
Balaji Tanniru <balajitanniru@Balajis-MacBook-Pro.local>
2026-01-24 18:38:37 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import Head from "./components/Head";
 export default function Home() {
   return (
-    <div className="flex  items-center justify-center bg-white font-sans ">
-       <Head/>
+    <div className="flex w-full items-center justify-center bg-white font-sans ">
+      
       <main className="flex min-h-screen  flex-col items-center justify-between  bg-white sm:items-start">
-     me
+      <Head/>
       </main>
     </div>
   );
