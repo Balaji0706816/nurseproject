e5be9722feca440f0e9297e784934081790bca45
Balaji Tanniru <balajitanniru@Balajis-MacBook-Pro.local>
2026-01-24 18:33:21 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import Head from "./components/Head";
 export default function Home() {
   return (
-    <div className="flex  items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen  flex-col items-center justify-between  bg-white dark:bg-black sm:items-start">
-      <Head/>
+    <div className="flex  items-center justify-center bg-white font-sans ">
+       <Head/>
+      <main className="flex min-h-screen  flex-col items-center justify-between  bg-white sm:items-start">
+     me
       </main>
     </div>
   );
